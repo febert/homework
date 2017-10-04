8823a7cafa671570c2acd2de4b850e423f1e7960
febert <frederik.ebert@mytum.de>
2017-10-04 09:27:40 -0700

    sc

---
+++ hw3/tensorflow_data/replay_1e7/conf.py
@@ -0,0 +1,14 @@
+import os
+import pdb
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+# local output directory
+OUT_DIR = current_dir + '/modeldata'
+
+configuration = {
+'experiment_name': 'rndaction_var10',
+'output_dir': OUT_DIR,      #'directory for model checkpoints.' ,
+'current_dir': current_dir,   #'directory for writing summary.' ,
+'jason_exp':'',
+'replay_buffer_size':int(1e7)
+}
